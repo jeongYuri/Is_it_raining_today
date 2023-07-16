96336ec88c87ae97f3a3a816287f8e4ea86caf7b
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-07-16 20:12:54 +0900

    git commit -m"15 commit"

---
+++ demoo/demo/src/main/java/com/example/crudw/demo/Controller/WeatherController.java
@@ -0,0 +1,92 @@
+package com.example.crudw.demo.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api")
+public class WeatherController {
+    @GetMapping("/weather")
+    public String getWeather() throws Exception {
+
+        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
+        String serviceKey = "PXL4ObzaxgbrcVcEe5c8QTzAdF5bD9Y714d3X2Tus6DgtWsj10nEfjP6lUL6Z%2FAPd2pM1XQhuvJx%2BWrEo5%2BLfw%3D%3D";
+        String nx = "55";
+        String ny = "127";
+        String base_date = "20230716";
+        String base_time = "0600";    //API 제공 시간을 입력하면 됨
+        String dataType = "JSON";    //타입 xml, json 등등 ..
+        String numOfRows = "10";
+
+        String url = apiUrl + "?serviceKey=" + serviceKey + "&nx=" + nx + "&ny=" + ny + "&base_date=" + base_date + "&base_time=" + base_time + "&dataType=" + dataType + "&numOfRows=" + numOfRows;
+
+        HashMap<String, Object> resultMap = getDataFromJson(url, "UTF-8", "get", "");
+        System.out.println("# RESULT :" + resultMap);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", resultMap);
+        return jsonObject.toString();
+    }
+    public HashMap<String,Object> getDataFromJson(String url,String encoding,String type,String jsonStr)throws  Exception{
+        boolean isPost = false;
+        if("post".equals(type)){
+            isPost=true;
+        }else{
+            url="".equals(jsonStr)?url:url+"?request=" +jsonStr;
+        }
+        return getStringFromURL(url,encoding,isPost,jsonStr,"application/json");
+    }
+    public HashMap<String,Object> getStringFromURL(String url,String encoding,boolean isPost,String parameter,String contentType)throws Exception{
+        URL apiURL = new URL(url);
+        HttpURLConnection conn= null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        HashMap<String ,Object> resultMap = new HashMap<String,Object>();
+        try{
+            conn=(HttpURLConnection) apiURL.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.setDoOutput(true);
+
+            if(isPost){
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type",contentType);
+                conn.setRequestProperty("Accept","*/*");
+            }else{
+                conn.setRequestMethod("GET");
+            }
+            conn.connect();
+            if(isPost){
+                bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(),"UTF-8"));
+                bw.write(parameter);
+                bw.flush();
+                bw=null;
+            }
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(),encoding));
+            String line = null;
+            StringBuffer result = new StringBuffer();
+            while((line=br.readLine())!=null)result.append(line);
+
+            ObjectMapper mapper = new ObjectMapper();
+            resultMap = mapper.readValue(result.toString(),HashMap.class);
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(url + "interfacefailed" + e.toString());
+        }finally{
+            if (conn!=null)conn.disconnect();
+            if(br!=null)br.close();
+            if(bw!=null)bw.close();
+        }
+
+        return resultMap;
+    }
+}
